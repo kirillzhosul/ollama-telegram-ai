7230f072af9b7953a71658dc4dbdf9552c3ffb0d
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2024-12-25 21:26:57 +0400

    feat: 🔧 Add configuration loading from environment, document settings

--- bot/settings.py
+++ bot/settings.py
@@ -1,18 +1,31 @@
-# Telegram
-TELEGRAM_TOKEN = ""
+"""
+Bot configuration.
 
-# Model
-START_USER_MESSAGE = ""
-SYSTEM_MESSAGE = """You are a bot
-{0}
+By default, will obtain required fields from environment variables,
+but you can override that with modifying defaults.
+
+RU: Используйте переменные окружения или поменяйте default полей
 """
 
+import os
 
+# Telegram token obtained from Bot Father
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", default="")
+
+# First message that will be sent to model as it was user.
+# If left blank or None will do not perform this operation.
+START_USER_MESSAGE = ""
+
+# Model system message.
+SYSTEM_MESSAGE = """You are a bot"""
+
+# Translator module, change from default `en` to something like `ru` to perform translation on user messages.
+# NOTICE: WILL USE TRANSLATOR API (NOT MODEL ONE)
 BASE_LANGUAGE = "en"
 
-# Ollama
-OLLAMA_API_HOST = "http://localhost:11434"
-OLLAMA_MODEL = "llama2:13b-chat"
+# Ollama server configuration
+OLLAMA_API_HOST = os.getenv("OLLAMA_API_HOST", default="http://localhost:11434")
+OLLAMA_MODEL = os.getenv("OLLAMA_BOT_MODEL", default="llama2:13b-chat")
 OLLAMA_MODEL_TEMPERATURE = 1
 OLLAMA_KEEP_ALIVE = "5m"
 # TODO: OLLAMA_INSTALL_MISSING_MODELS
